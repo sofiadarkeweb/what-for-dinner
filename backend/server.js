1bbbc59065498d362b5a1c05f96009074a8cf130
SofiaDarke <sofiadarke@earthpeople.se>
2022-03-19 09:19:15 +0100

    Set up express and run on port 5000

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,7 @@
-console.log("hello you");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = 5000;
+
+const app = express();
+
+app.listen(port, () => console.log(`server started on ${port}`));
