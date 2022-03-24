32f94bf38ac84c5bc7a9699e2ff91a93df06060d
SofiaDarke <sofiadarke@earthpeople.se>
2022-03-24 13:37:32 +0100

    change production

--- backend/server.js
+++ backend/server.js
@@ -33,18 +33,18 @@ app.use("/api/food", require("./routes/foodRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
 // Serve frontend
+if ("production")
+	if (process.env.NODE_ENV === "production") {
+		app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-	app.get("*", (req, res) =>
-		res.sendFile(
-			path.resolve(__dirname, "../", "frontend", "build", "index.html")
-		)
-	);
-} else {
-	app.get("/", (req, res) => res.send("Please set to production"));
-}
+		app.get("*", (req, res) =>
+			res.sendFile(
+				path.resolve(__dirname, "../", "frontend", "build", "index.html")
+			)
+		);
+	} else {
+		app.get("/", (req, res) => res.send("Please set to production"));
+	}
 
 app.use(errorHandler);
 
